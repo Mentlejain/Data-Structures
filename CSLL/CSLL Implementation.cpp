ce46ac49dbb65b09d9a8731bdfcc254b8f0b6ecb
Mr-Mentlegen <47449331+Mr-Mentlegen@users.noreply.github.com>
2019-10-12 23:32:47 +0530

    Add files via upload

---
+++ CSLL/CSLL Implementation.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<conio.h>
+#include"CSLL.h"
+using namespace std;
+template <class N>
+void menu(CSLL<N> &L)
+{
+	int n;
+	char ch;
+	N x;
+	cout<<"Select DLL operation"<<endl;
+	cout<<"1.Insert"<<endl;
+	cout<<"2.Reverse DLL"<<endl;
+	cout<<"3.Display"<<endl;
+	cout<<"4.Delete"<<endl;
+	cout<<"5.Search by index"<<endl;
+	cout<<"6.Search by value"<<endl;
+	cout<<"7.Display alternate elements"<<endl;
+	cout<<"8.Display options"<<endl;
+	do 
+	{
+		switch (getch()-48)
+		{
+			case 1:
+			{
+				cout<<"1=Beginning -1=End Else any position: ";
+				cin>>n;
+				cout<<"Enter data: ";
+				cin>>x;
+				L.insert(n,x);
+				L.display();
+				break;
+			}
+			case 2:
+			{
+				L.reverse();
+				L.display();
+				break;
+			}
+			case 3:
+			{
+				L.display();
+				break;
+			}
+			case 4:
+			{
+				cout<<"1=Beginning -1=End Else any position";
+				cin>>n;
+				L.del(n);
+				L.display();
+				break;
+			}
+			case 5:
+			{
+				cout<<"Enter index"<<endl;
+				cout<<"1=Beginning -1=End Else any position";
+				cin>>n;
+				if(n>L.length())
+				{
+					cout<<"Index not found"<<endl;
+				}
+				else
+				{
+					cout<<L.svbi(n)<<endl;
+				}
+				break;
+			}
+			case 6:
+			{
+				cout<<"Enter value:";
+				cin>>n;
+				n=L.sibv(n);
+				if(n>=1)
+				{
+					cout<<"Value found at node: "<<n<<endl;
+				}
+				else if(n==0)
+				{
+					cout<<"List is empty"<<endl;
+				}
+				else if(n==-1)
+				{
+					cout<<"Value not found"<<endl;
+				}
+				break;
+			}
+			case 7:
+			{
+				L.displayalt();
+				break;
+			}
+			case 8:
+			{
+				cout<<"1.Insert"<<endl;
+				cout<<"2.Reverse DLL"<<endl;
+				cout<<"3.Display"<<endl;
+				cout<<"4.Delete"<<endl;
+				cout<<"5.Search"<<endl;
+				cout<<"6.Display alternate elements"<<endl;
+				cout<<"7.Display options"<<endl;
+				break;
+			}
+			default:
+			{
+				cout<<"Invalid option"<<endl;
+			}
+		}
+		cout<<"Enter y to continue else end \r";
+		ch=(char)getch();
+		cout<<"                                               \r"<<"\b";
+	}
+	while (ch=='y' or ch=='Y');
+}
+int main()
+{
+	CSLL<int> L[3];
+	do 
+	{
+		cout<<"1. List 1"<<endl;
+		cout<<"2. List 2"<<endl;
+		cout<<"3. Concatenate both lists"<<endl;
+		cout<<"4. Exit"<<endl;
+		switch (getch()-48)
+		{
+			case 1:
+			{
+				menu(L[0]);
+				break;
+			}
+			case 2:
+			{
+				menu(L[1]);
+				break;
+			}
+			case 3:
+			{
+				L[2]=(L[0]+L[1]);
+				L[2].display();
+				break;
+			}
+		
+			case 4:
+			{
+				exit(3000);
+			}
+		}
+	}
+	while(true);
+	getch();
+}
---
+++ CSLL/CSLL.h
@@ -0,0 +1,311 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+template <class N>
+struct node
+{
+	N info;
+	node *next=NULL;
+};
+template <class N>
+class CSLL
+{
+	private:
+		int l;
+		node<N>* tail=NULL;
+	public:
+		node<N>* tailptr();
+	//	~CSLL();
+		CSLL();//
+		CSLL(CSLL &ob);//
+		bool isempty(); //
+		void reverse();//
+		int length();//
+		void insert(int val, N info);//
+		void display();//
+		void del(int);//
+		N svbi(int);
+		int sibv(N);
+		void displayalt();//
+		CSLL operator+(CSLL &L);
+};
+template <class N>
+CSLL<N>::CSLL()
+{
+	tail=NULL;
+	l=0;
+}
+template <class N>
+node<N>* CSLL<N>::tailptr()
+{
+	return tail;
+}
+template <class N>
+int CSLL<N>::length()
+{
+	return l;
+}
+template <class N>
+bool CSLL<N>::isempty()
+{
+	if(tail==NULL)
+		return true;
+	else
+		return false;
+}
+template <class N>
+int CSLL<N>::sibv(N val)
+{
+	if(isempty())
+	{
+	//	cout<<"List is empty"<<endl;
+		return 0;
+	}
+	else
+	{
+		int ind=0;
+		node<N>* p;
+		p=tail;
+		while(p!=NULL)
+		{
+			if(p->info==val)
+			{
+				return ind;
+			}
+			else
+			{
+				ind++;
+			}
+		}
+		return -1;
+	}
+}
+template <class N>
+N CSLL<N>::svbi(int pos)
+{
+	if(isempty())
+	{
+		cout<<"List is empty"<<endl;
+	}
+	else
+	{
+		N val;
+		if(pos==1)
+		{
+			val=tail->next->info;
+			return val;
+		}
+		else if(pos==-1 or pos>l)
+		{
+			node<N>* p;
+			p=tail->next;
+			while(p->next!=NULL)
+			{
+				p=p->next;
+			}
+			val=p->info;
+			delete p;
+			return val;
+		}
+		else if(pos<=l)
+		{
+			N val;
+			node<N>* p;
+			p=tail->next;
+			for(int i=1;i<pos and p->next!=NULL;i++)
+			{
+				p=p->next;
+			}
+			val=p->info;
+			delete p;
+			return val;
+		}
+	}
+}
+template <class N>
+void CSLL<N>::insert(int pos,N inf)
+{
+	node<N> *n=new node<N>;
+	n->info=inf;
+	n->next=NULL;
+	if(pos==1)
+	{
+		if(tail==NULL)
+		{
+			tail=n;
+			tail->next=tail;
+			l++;
+			n=NULL;
+			delete n;
+		}
+		else
+		{
+			
+			n->next=tail->next;
+			tail->next=n;
+			n=NULL;
+			delete n;
+			l++;
+		}
+	}
+	else if(pos==-1 or pos>l)
+	{
+		if(tail==NULL)
+		{
+			tail=n;
+			tail->next=tail;
+			l++;
+			n=NULL;
+			delete n;
+		}
+		else
+		{
+			n->next=tail->next;
+			tail->next=n;
+			tail=tail->next;
+			l++;
+		}
+	}
+	else if(pos<=l)
+	{
+		node<N>* p;
+		p=tail->next;
+		for(int i=1;i<pos-1 and p!=tail;i++)
+		{
+			p=p->next;
+		}
+		n->next=p->next;
+		p->next=n;
+		p=NULL;
+		delete p;
+		l++;
+	}
+}
+template <class N>
+void CSLL<N>::del(int pos)
+{
+	if(isempty())
+		cout<<"List is empty"<<endl;
+	else if(tail->next==tail)
+	{
+		tail->next=NULL;
+		tail==NULL;
+		l--;
+	}
+	else
+	{
+		if(pos==1)
+		{
+			node<N>* p;
+			p=tail->next;
+			tail->next=p->next;
+			p->next=NULL;
+			p=NULL;
+			delete p;
+			l--;;
+		}
+		else if(pos==-1)
+		{
+			node<N>* p=new node<int>;
+			p=tail->next;
+			while(p->next!=tail)
+			{
+				p=p->next;
+			}
+			p->next=tail->next;
+			tail->next=NULL;
+			tail=p;
+			p=NULL;
+			delete p;
+			l--;
+		}
+		else
+		{
+			node<N>* p=new node<N>;
+			node<N>* t=new node<N>;
+			p=tail->next;
+			for(int i=1;i<pos and p!=tail;i++)
+			{
+				t=p;
+				p=p->next;
+			}
+			t->next=p->next;
+			p->next=NULL;
+			p=t=NULL;
+			delete p;
+			delete t;
+			l--;
+		}
+	}
+}
+template <class N>
+void CSLL<N> ::display()
+{
+	node<N>* p=new node<N>;
+	p=tail->next;
+	do
+	{
+		cout<<"["<<p->info<<"]"<<"->";
+		p=p->next;
+	}while(p!=tail->next);
+	cout<<endl;
+}
+template <class N>
+void CSLL<N>::reverse()
+{
+	if(isempty())
+	{
+		cout<<"List is empty"<<endl;
+	}
+	else
+	{
+		node<N>* n;
+		node<N>* p;
+		node<N>* c;
+		p=tail->next;
+		c=p->next;
+		tail=tail->next;
+		do
+		{
+			n=c->next;
+			c->next=p;
+			p=c;
+			c=n;
+		}while(p!=tail->next);
+	}
+}
+template <class N>
+void CSLL<N> ::displayalt()
+{
+	node<N>* p=new node<N>;
+	p=tail->next;
+	do
+	{	
+		cout<<"["<<p->info<<"] ";
+		p=p->next->next;
+	}
+	while(p!=tail and p->next!=tail);
+	cout<<endl;
+}
+template <class N>
+CSLL<N> CSLL<N>:: operator+ (CSLL<N> &L)
+{
+	CSLL<int>res;
+	node<N>*p;
+	p=tail->next;
+	do
+	{
+		res.insert(-1,p->info);
+		p=p->next;
+	}while(p!=tail);
+	p=L.tailptr()->next;
+	do
+	{
+		res.insert(-1,p->info);
+		p=p->next;
+	}while(p!=L.tailptr());
+	p->next=tail->next;
+	p=NULL;
+	delete p;
+	return res; 	
+}
